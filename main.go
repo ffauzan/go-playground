3bca245d56ddc0ff73ad52cb19c25f6c132dd1e0
ffauzan <f.fauzan@mail.ugm.ac.id>
2022-12-05 08:48:24 +0700

    placeholder

--- main.go
+++ main.go
@@ -1,51 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	fmt.Println("aa")
-	arr := [][]int32{
-		{112, 42, 83, 119},
-		{56, 125, 56, 49},
-		{15, 78, 101, 43},
-		{62, 98, 114, 108},
-	}
-
-	flippingMatrix(arr)
-}
-
-func flippingMatrix(matrix [][]int32) int32 {
-	// Write your code here
-	quadranSize := len(matrix) / 2
-
-	// row
-
-	for i := 0; i < len(matrix); i++ {
-		// First half sum
-		firstHalfRowSum := 0
-
-		sectHalfRowSum := 0
-
-		for j := 0; j < len(matrix); j++ {
-			if i < len(matrix)/2 {
-				firstHalfRowSum += int(matrix[i][j])
-			} else {
-				sectHalfRowSum += int(matrix[i][j])
-			}
-		}
-
-		// switch if needed
-		if firstHalfRowSum < sectHalfRowSum {
-			// switch
-
-		}
-
-	}
-
-}
-
-func reverseRow(row []int32) []int32 {
 
 }
--- mock-2/main.go
+++ mock-2/main.go
@@ -18,7 +18,7 @@ import (
 
 func flippingMatrix(matrix [][]int32) int32 {
 	// Write your code here
-
+	return 0
 }
 
 func main() {
